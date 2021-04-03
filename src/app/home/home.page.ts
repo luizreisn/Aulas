96f03e0c41e2318ac2c1a9f72db05b3fcde4206a
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-02 22:43:58 -0300

    Aula 02 - Exercicios Praticos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,3 +10,30 @@ export class HomePage {
   constructor() {}
 
 }
+
+
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'menu-example',
+  templateUrl: 'home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class MenuExample {
+
+constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+}
