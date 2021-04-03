e7a328942a02a27c6e398a894679840c3986ff1c
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-03 00:28:31 -0300

    Aula 04 - Diretivas e Pipes (Exemplos)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { NodeWithI18n } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    public estudante={
+      nome: 'Luiz Gabriel',
+      nota: 3
+    }
 
+    public provas=[
+      {nome: 'Prova N1', nota: 6.8},
+      {nome: 'Prova N2', nota: 8.9},
+      {nome: 'Prova N3', nota: 7.0},
+      {nome: 'Prova Final', nota: 6.9}
+    ];
+
+  constructor() {}
+    public incrementa(){
+      this.estudante.nota ++;
+    }
+
+    public entregas=[
+      {nome: 'TV', 
+       diasDesdeACompra: 7, 
+       diasEstimadosEntrega: 8, 
+       status: 'onRoute'},
+
+      {nome: 'Nintendo Switch', 
+       diasDesdeACompra: 15,
+       diasEstimadosEntrega: 15,
+       status: 'delivered'},
+
+      {nome: 'JBL', 
+       diasDesdeACompra: 6,
+       diasEstimadosEntrega: 4,
+       status: 'onRoute'},
+
+      {nome: 'Máquina de Lavar',
+       diasDesdeACompra: 2,
+       diasEstimadosEntrega: 15,
+       status: 'preparing'},
+       
+      {nome: 'MousePad',
+       diasDesdeACompra: 4,
+       diasEstimadosEntrega: 2,
+       status: 'onRoute'}
+    ]
 }
