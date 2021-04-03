caf97a86a9bea9df48b6e1d37e7bf0ff67c7d4ea
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-02 22:51:05 -0300

    Aula 03 - Exercicios Praticos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,86 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public contador = {
+    atual: 0,
+    maximo: 0
+  }
+  public conta={
+    atual: 0,
+    input: null
+  }
 
+  public escolha ={
+    de: "",
+    para:""
+  }
+  
+  public input={
+    de: null,
+    para: null
+  }
+
+  public contadorProf = {
+    atual: 0,
+    maximo: 0
+  }
+
+  constructor() {}
+    public incrementar(){
+      if(this.contador.atual == this.contador.maximo){
+        this.contador.maximo ++;
+        this.contador.atual ++;
+      }else{
+        this.contador.atual ++;
+      }
+    }
+
+    public decrementa(){
+      this.contador.atual --;
+    }
+
+    public resetar(){
+      this.contador.atual = 0,
+      this.contador.maximo = 0;
+    }
+
+    public sacar(){
+      if(this.conta.input > this.conta.atual){
+        this.conta.atual = 0;
+      }else{
+        this.conta.atual -= this.conta.input;
+      }
+      this.conta.input = null;
+    }
+
+    public depositar(){
+      this.conta.atual += this.conta.input;
+      this.conta.input = null;
+    }
+
+    public sacarTudo(){
+      this.conta.atual = 0;
+      this.conta.input = null;
+
+    }
+
+    public calcula(){
+      
+    }
+
+    public incrementarProf(){
+      this.contadorProf.atual++;
+      this.contadorProf.maximo = Math.max(this.contadorProf.maximo, this.contadorProf.atual);
+    }
+
+    public decrementaProf(){
+      this.contadorProf.atual --;
+    }
+
+    public resetarProf(){
+      this.contadorProf.atual = 0,
+      this.contadorProf.maximo = 0;
+    }
+
+    
 }
