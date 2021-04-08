4227b08fe8e04f9ba6bb238b673adc84bcbc3b24
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-08 09:54:51 -0300

    sfgdg

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+  interface Task{
+    name: string;
+    done: boolean;
+  }
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +11,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public tasks: Task[]= []
+  public newTaskName = '';
+  public progress = 0;
+
   constructor() {}
+
+  public addNewTask(){
+    if(this.newTaskName.trim().length == 0)
+    this.tasks.push({
+      name: this.newTaskName.trim(),
+      done: false
+    })
+    this.newTaskName = '';
+    this.calculateProgress();
+  }
+
+  public calculateProgress(){
+    //let completedTasks = 0;
+    //for(let task of this.tasks){
+    //  if(task){
+    //    completedTasks++;
+    //  }
+    //}
+    //this.progress = completedTasks / this.tasks.length;
+  
+    this.progress = this.tasks.filter(function(t){
+      return t.done;
+    }).length / this.tasks.length
+  } 
+
+  public removeTask(toRemove: Task){
+    const index = this.tasks.findIndex(t => t == toRemove)
+  }
 
 }
