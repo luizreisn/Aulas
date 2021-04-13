399c75874caff931dc1450ed7083b2afa8c572df
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-13 15:25:20 -0300

    voltando a master ao normal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-  interface Task{
-    name: string;
-    done: boolean;
-  }
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,38 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  public tasks: Task[]= []
-  public newTaskName = '';
-  public progress = 0;
-
   constructor() {}
-
-  public addNewTask(){
-    if(this.newTaskName.trim().length == 0)
-    this.tasks.push({
-      name: this.newTaskName.trim(),
-      done: false
-    })
-    this.newTaskName = '';
-    this.calculateProgress();
-  }
-
-  public calculateProgress(){
-    //let completedTasks = 0;
-    //for(let task of this.tasks){
-    //  if(task){
-    //    completedTasks++;
-    //  }
-    //}
-    //this.progress = completedTasks / this.tasks.length;
-  
-    this.progress = this.tasks.filter(function(t){
-      return t.done;
-    }).length / this.tasks.length
-  } 
-
-  public removeTask(toRemove: Task){
-    const index = this.tasks.findIndex(t => t == toRemove)
-  }
 
 }
