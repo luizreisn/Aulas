162346c14088681a041c69836b49218937d18776
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-13 21:41:19 -0300

    Exemplos de Alertas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public alertCtrl: AlertController;
+
+  constructor(ac: AlertController) {
+    this.alertCtrl = ac;
+  }
+
+  public showAlert(){
+    const alert = this.alertCtrl.create({
+      header: 'Ola Mundo',
+      message: 'Esse é o meu primeiro Alerta',
+      buttons: ['Ok', 'Beleza', 'Falou']
+    }).then(function(alert){
+      alert.present();
+    })
+  }
+
+  public async showAlert2(){
+    const alert = await this.alertCtrl.create({
+      header: 'Ola Mundo',
+      message: 'Esse é o meu segundo Alerta',
+      buttons: ['Ok', 'Beleza', 'Falou']
+    })
+      alert.present();
+  }
+
+  public async showAlert3(){
+    const alert = await this.alertCtrl.create({
+      header: 'Tem certeza?',
+      message: 'Essa operação não pode ser desfeita.',
+      buttons: [{
+        text: 'Ok',
+        handler: () => console.log('clicou em OK')
+      },{
+        text: 'Cancelar',
+        handler: function(){
+          console.log('clicou em Cancelar')
+        }
+      }]
+    })
+      alert.present();
+  }
+
+
 
 }
