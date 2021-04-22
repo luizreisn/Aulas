1b19b4b07f4347fd9c0f4ea02e2e4947cc825188
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-04-22 18:35:56 -0300

    efeito like

--- src/app/home/feed/feed.page.ts
+++ src/app/home/feed/feed.page.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
+  public posts = [
+    {
+      id: 22,
+      curtido: true,
+      usado: false,
+      user: 'luizreisn',
+      descricao: 'Foto legal'
+    },{
+      id: 7,
+      curtido: false,
+      usado: false,
+      user: 'luizreisn',
+      descricao: 'Foto media'
+    },{
+      id: 3,
+      curtido: true,
+      usado: false,
+      user: 'luizreisn',
+      descricao: 'Foto ruim'
+    },{
+      id: 78,
+      curtido: false,
+      usado: false,
+      user: 'luizreisn',
+      descricao: 'Foto mais ou menos'
+    },{
+      id: 45,
+      curtido: true,
+      usado: false,
+      user: 'luizreisn',
+      descricao: 'Foto boa'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleLike(post){
+    post.usado = true;
+    post.curtido = !post.curtido
   }
 
 }
